9074ab39fb2f43aa1beae28da7295c67e71815c4
Logan Kim <logan@rootone.com>
2019-01-02 16:22:07 +0900

    api app 추가.

---
+++ api/middle_wares.py
@@ -0,0 +1,28 @@
+import logging
+import re
+import time
+import uuid
+
+from django.utils.deprecation import MiddlewareMixin
+
+logger = logging.getLogger(__name__)
+
+
+class LoggingMiddleware(MiddlewareMixin):
+
+    def __init__(self, get_response=None):
+        super().__init__(get_response)
+
+    def process_request(self, request):
+        request.start_time = time.time()
+        request.uuid = str(uuid.uuid4())
+        body = re.sub(r"\s+", "", request.body.decode('utf-8'), flags=re.UNICODE)
+        logger.debug(
+            f"[{request.uuid}] request: {request.method} {request.get_full_path()}{request.GET.urlencode()} {body}")
+
+    def process_response(self, request, response):
+        elapsed_time = time.time() - request.start_time
+        uuid = request.uuid
+        logger.debug(
+            f"[{request.uuid}] response: {request.method} {request.get_full_path()}{request.GET.urlencode()} ({elapsed_time} s)")
+        return response
